ed9f58d5aca07e5ee8038589f28fb87cf3a46d84
Javed <javedalikhan50@gmail.com>
2022-08-24 12:45:26 +0530

    added ff method

--- src/main/java/com/primus/primus/Config/AppConfig.java
+++ src/main/java/com/primus/primus/Config/AppConfig.java
@@ -1,11 +1,13 @@
 package com.primus.primus.Config;
 
 import com.configcat.ConfigCatClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
+    @Bean
     public ConfigCatClient configCatClient(){
         return ConfigCatClient.newBuilder().build("ScjZCHC7nE-HInm60zKy3w/IF-jMFyoBkWADOD-6dOMLA");
     }
--- src/main/java/com/primus/primus/Controller/ConfigController.java
+++ src/main/java/com/primus/primus/Controller/ConfigController.java
@@ -1,6 +1,27 @@
 package com.primus.primus.Controller;
 
+import com.configcat.ConfigCatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ConfigController {
 
+    @Autowired
+    ConfigCatClient configCatClient;
 
+    @GetMapping(value = "api/featureflag")
+    public String getFeatureFlag(){
+        boolean isFeatureEnable = configCatClient
+                .getValue(Boolean.class,"isMyFirstFeatureEnabled",false);
+
+        if(isFeatureEnable){
+            System.out.println("Feature Flag is ON \uD83D\uDE00 ");
+        }
+        else{
+            System.out.println("Feature Flag is OFF \uD83D\uDE14");
+        }
+        return "isFeatureEnable : " + isFeatureEnable;
+    }
 }
